c6e5a6eb0339d8054c61eccaff802e802372627b
AryanKum99 <aryan242003@gmail.com>
2023-09-19 18:52:28 +0530

    added auth in create team route

--- routes/team.js
+++ routes/team.js
@@ -6,6 +6,6 @@ const auth = require('../middleware/authmiddleware');
 router.route('/getTeam')
     .get(auth, team.getTeam)
 router.route('/createTeam')
-    .post(team.makeTeam)
+    .post(auth, team.makeTeam)
 
 module.exports = router;
