8206254f1758955bd33fc6d34403a26d1b10deca
Smriti Doneria <smritidoneria@gmail.com>
2023-09-22 09:37:33 +0530

    new changes

--- controllers/roundTwo/file.js
+++ controllers/roundTwo/file.js
@@ -4,6 +4,7 @@ const catchAsync = require("../../utils/catchAsync");
 const path = require('path');
 
 exports.valuationafterR1 = catchAsync(async (req, res, next) => {
+  
   try {
     
     const excelFile = path.join(__dirname, 'investor_smriti.xlsx');
@@ -37,11 +38,14 @@ exports.valuationafterR1 = catchAsync(async (req, res, next) => {
   } catch (error) {
     console.error('An error occurred:', error);
   }
+  
+  
 });
 
 
 
 exports.valuationaftercrises = catchAsync(async (req, res, next) => {
+  /*
   try {
     
     const excelFile = path.join(__dirname, 'investor_smriti.xlsx');
@@ -52,19 +56,22 @@ exports.valuationaftercrises = catchAsync(async (req, res, next) => {
 
    
     const jsonData = XLSX.utils.sheet_to_json(worksheet);
-
+    console.log(jsonData);
     
     const teamnumber = jsonData.map(row => row['__EMPTY']);
     const valuation_after_crises = jsonData.map(row => row['__EMPTY_5']);
     const VP_after_crises=jsonData.map(row => row['__EMPTY_6']);
     
-    for (let i = 0; i < teamnumber.length; i++) {
+    for (let i = 1; i <teamnumber.length; i++) {
       const number = teamnumber[i];
       const valuation = valuation_after_crises[i];
-      const vps=VP_after_crises[i];
-
       
-      await Team.findOneAndUpdate({ teamnumber: number }, { $set: { valuation: valuation } });
+      const vps1 = VP_after_crises[i];
+      const vps = !isNaN(vps1) ? parseInt(vps1) : 0; // If it's not a number, set it to 0 or handle it differently
+     // console.log("vps1",vps1,typeof(vps1));
+      //console.log("vps",vps,typeof(vps));
+      
+      await Team.findOneAndUpdate({ teamnumber: number }, { $set: { valuationaftercrises: valuation } });
       await Team.findOneAndUpdate({ teamnumber: number }, { $set: { vps:vps } });
 
       
@@ -75,5 +82,41 @@ exports.valuationaftercrises = catchAsync(async (req, res, next) => {
   } catch (error) {
     console.error('An error occurred:', error);
   }
-});
+  */
+  try {
+
+    const excelFile = path.join(__dirname, 'investor1.xlsx');
+    const workbook = XLSX.readFile(excelFile);
+
+    const sheetName = workbook.SheetNames[0];
+    const worksheet = workbook.Sheets[sheetName];
+
+
+    const jsonData = XLSX.utils.sheet_to_json(worksheet);
+    console.log(jsonData);
+
+    const teamnumber = jsonData.map(row => row['teamno']);
+    //console.log(teamnumber[10]);
+    const valuation_after_crises = jsonData.map(row => row['Valuation_a_crises']);
+    //console.log(valuation_after_crises[10]);
+    const VP_after_crises = jsonData.map(row => row['Valuation_a_crises']);
+   // console.log(VP_after_crises[10]);
+    for (let i = 1; i <=teamnumber.length; i++) {
+      const number = teamnumber[i-1];
+      const valuation = valuation_after_crises[i-1];
+      
+      const vps1 = VP_after_crises[i-1];
+      const vps = !isNaN(vps1) ? parseInt(vps1) : 0; // If it's not a number, set it to 0 or handle it differently
+     // console.log("vps1",vps1,typeof(vps1));
+      //console.log("vps",vps,typeof(vps));
+      
+      await Team.findOneAndUpdate({ teamNumber: number }, { $set: { valuationaftercrises: valuation } });
+      await Team.findOneAndUpdate({ teamNumber: number }, { $set: { vps:vps } });
+    console.log("Valuation updated successfully.");
+    
+  } 
+  res.status(200).json("Valuation updated successfully."); 
+} catch (error) {
+  console.error('An error occurred:', error);
+}});
 
--- controllers/roundTwo/quiz.js
+++ controllers/roundTwo/quiz.js
@@ -2,40 +2,37 @@ const XLSX = require('xlsx');
 const Team = require('../../models/teamModel');
 const catchAsync = require("../../utils/catchAsync");
 const path = require('path');
-const { valuationaftercrises } = require('./file');
 
 exports.quiz = catchAsync(async (req, res, next) => {
-    try {
-    
-        const excelFile = path.join(__dirname, 'investor_smriti.xlsx');
-        const workbook = XLSX.readFile(excelFile);
-        
-        const sheetName = workbook.SheetNames[3];
-        const worksheet = workbook.Sheets[sheetName];
-    
-       
-        const jsonData = XLSX.utils.sheet_to_json(worksheet);
-    
-        
-        const teamnumber = jsonData.map(row => row['__EMPTY']);
-       
-        const VP_after_quiz=jsonData.map(row => row['__EMPTY_9']);
-        
-        for (let i = 0; i < teamnumber.length; i++) {
-          const number = teamnumber[i];
-       
-          const vps=VP_after_quiz[i];
-    
-          
-          
-          await Team.findOneAndUpdate({ teamnumber: number }, { $set: { vps:vps } });
-    
-          
-        }
-    
-        
-        console.log("Valuation_after_crises updated successfully.");
-      } catch (error) {
-        console.error('An error occurred:', error);
-      }
+  try {
+
+    const excelFile = path.join(__dirname, 'quizFile.xlsx');
+    const workbook = XLSX.readFile(excelFile);
+
+    const sheetName = workbook.SheetNames[0];
+    const worksheet = workbook.Sheets[sheetName];
+
+
+    const jsonData = XLSX.utils.sheet_to_json(worksheet);
+    console.log(jsonData);
+
+    const teamNumber = jsonData.map(row => row['Team No']);
+    console.log(teamNumber);
+    const scores = jsonData.map(row => row['Score']);
+    console.log(scores);
+
+    for (let i = 0; i < teamNumber.length; i++) {
+      const number = teamNumber[i];
+      const quizScore = scores[i];
+      const team = await Team.findOne({ teamNumber: number });
+      console.log(team.vps);
+      const newVps = team.vps + Math.round(((quizScore *2)/10 ));
+      const newValuation = newVps * 100;
+      await Team.findOneAndUpdate({ teamNumber: number }, { $set: { vps: newVps, valuation: newValuation } });
+    }
+    console.log("Valuation updated successfully.");
+    res.status(200).json("Valuation updated successfully.");
+  } catch (error) {
+    console.error('An error occurred:', error);
+  }
 });
